ea7c99fd4cfddd7dabf7eb91c99c5ee3aab88a7b
sLeDa <sleda@hotmail.com>
2023-02-22 13:11:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,46 +8,46 @@ freezer = Freezer(app)
 def index():
     return render_template('index.html')
 
-@views.route('/blog/')
+@app.route('/blog/')
 def blog():
     return render_template('blog.html')
 
 
-@views.route('/pentest/')
+@app.route('/pentest/')
 def pentest():
     return render_template('pentest.html')
 
 
-@views.route('/whoami/')
+@app.route('/whoami/')
 def preojects():
     return render_template('whoami.html')
 
 
-@views.route('/necmo/')
+@app.route('/necmo/')
 def necmo_bot():
     return render_template('necmo.html')
 
 
-@views.route('/100322/')
+@app.route('/100322/')
 def kalp():
     return render_template('100322.html')
 
 
-@views.route('/blog/network-protocols')
+@app.route('/blog/network-protocols')
 def article_network_protocols():
     return render_template('network-p.html')
 
 
-@views.route('/blog/network-technologies')
+@app.route('/blog/network-technologies')
 def article_network_technologies():
     return render_template('network-technologies.html')
 
-@views.route('/blog/cs-aviation')
+@app.route('/blog/cs-aviation')
 def article_cs_aviation():
     return render_template('cs-aviation.html')
 
 
-@views.route('/blog/whiplash')
+@app.route('/blog/whiplash')
 def whiplash():
     return render_template('whiplash.html')
 
